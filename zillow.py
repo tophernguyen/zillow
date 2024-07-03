736ee52a5fa364c159d46cdc73a1e93dde5e042c
Topher Nguyen <data@tophernguyen.com>
2024-07-02 23:44:32 -0500

    Finish linear regression. Did not like due to lack of variables.  Modded zillow.py to give me original request.json for more in-depth analysis. Added timestamp to file-naming

--- zillow.py
+++ zillow.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import requests
 import warnings
+from datetime import date
+import json
 
 # settings
 warnings.filterwarnings("ignore")
@@ -35,11 +37,19 @@ def get_listings(api_key, listing_url):
     return requests.request("GET", url, params=querystring)
 
 def process_listing(listing_response):
+    today = date.today().strftime("%y.%m.%d")
     print(listing_response.json().keys())
+
     num_of_properties = listing_response.json()["data"]["categoryTotals"]["cat1"]["totalResultCount"]
     print("Count of properties:", num_of_properties)
     df_listings = pd.json_normalize(listing_response.json()["data"]["cat1"]["searchResults"]["mapResults"])
-    df_listings.to_csv("zillow.csv", index = False)
+    df_listings.to_csv("zillow." + today +".csv", index = False)
+
+    text = json.dumps(listing_response.json(),sort_keys = True, indent = 4)
+
+
+    with open("zillow."+ today +".json", "w", encoding="utf-8") as f:
+        f.write(text)
 
 
 if __name__ == '__main__':
